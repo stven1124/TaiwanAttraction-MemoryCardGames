5586a050894a833217e1dc4730ce9c2f9777f1ed
stven1124 <stven1124@gmail.com>
2022-04-16 15:40:05 +0800

    1.增加橫屏判斷

--- js/all.js
+++ js/all.js
@@ -199,7 +199,7 @@ function shuffleCards() {
 
 /* 遊戲開始時的彈跳視窗 */
 function startAlert() {
-  if (window.orientation === 90 || window.orientation === -90) {
+  if (window.orientation === 90 || window.orientation === -90 || window.matchMedia("(orientation: landscape)").matches) {
     swal({
       closeOnClickOutside: false,
       closeOnEsc: false,
